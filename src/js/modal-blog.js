571a230132580f6d2dabfd67416f004ddcb3ed0d
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-04-06 09:32:45 +0200

    fix js

--- src/js/modal-blog.js
+++ src/js/modal-blog.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-modal-blog-open]'),
+    closeModalBtn: document.querySelector('[data-modal-blog-close]'),
+    modal: document.querySelector('[data-modal-blog]'),
   };
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
