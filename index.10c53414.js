dce8e27d7629f96397976c8a5c1ece4ade79f928
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-04-06 08:50:26 +0000

    Deploying to gh-pages from @ MariuszZiental/group-4-team-project@3bc0bcea0524849b36cac0806c48888dc1342267 🚀

---
+++ index.10c53414.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-modal-blog-open]"),closeModalBtn:document.querySelector("[data-modal-blog-close]"),modal:document.querySelector("[data-modal-blog]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.10c53414.js.map
---
+++ index.4665f0e7.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-modal-contact-open]"),closeModalBtn:document.querySelector("[data-modal-contact-close]"),modal:document.querySelector("[data-modal-contact]")};function t(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",t),o.closeModalBtn.addEventListener("click",t)})();
+//# sourceMappingURL=index.4665f0e7.js.map
